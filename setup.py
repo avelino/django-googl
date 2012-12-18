72b1ac3a06b8266a361ed752a2b391c0906bd06d
Thiago Avelino <thiagoavelinoster@gmail.com>
2012-12-18 20:54:32 -0200

    Fix bug install (pip) in python setup

--- setup.py
+++ setup.py
@@ -1,32 +1,35 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 
 version = '0.1'
 description = u"Django goo.gl app"
 
+classifiers = ['Environment :: Plugins',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',]
+
+long_description = open('README.md').read()
+
 setup(name='django-googl',
     version = version,
     description = description,
-    long_description = (''),
-    classifiers = [
-      'Environment :: Plugins',
-      'Framework :: Django',
-      'Intended Audience :: Developers',
-      'License :: OSI Approved :: BSD License',
-      'Operating System :: OS Independent',
-      'Programming Language :: Python',
-      'Topic :: Software Development :: Libraries :: Python Modules',
-      ],
+    long_description = long_description,
+    classifiers = classifiers,
     keywords = 'django apps googl google api ',
     author = 'Thiago Avelino',
     author_email = 'thiagoavelinoster@gmail.com',
-    url = 'https://github.com/avelino/django-googl',
+    url = 'https://github.com/avelino/django-googl/',
     download_url = "https://github.com/avelino/django-googl/tarball/master",
     license = 'BSD',
-    packages = ['googl'],
+    packages = find_packages(),
     package_dir = {'googl': 'googl'},
+    include_package_data = True,
 )
