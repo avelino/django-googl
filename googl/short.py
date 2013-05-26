6dcd9c39ec5ee287054e704d8f2133a38ae87976
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-05-26 09:10:03 -0300

    code review, improving the logic

--- googl/short.py
+++ googl/short.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import urllib, urllib2, json
-
+from django.core.validators import URLValidator, ValidationError
 
 
 class GooglUrlShort(object):
@@ -10,31 +9,40 @@ class GooglUrlShort(object):
 
     def __init__(self, url):
         self.url = url
+        if isinstance(url, unicode):
+            self.url = url.encode('utf-8')
+
+        url_validator = URLValidator()
+        try:
+            url_validator(self.url)
+        except ValidationError:
+            return u""
+
 
     def short(self, all_response = False):
         header = { "Content-Type": "application/json" }
         params = { "longUrl": self.url }
 
         try:
-            response = urllib2.urlopen(urllib2.Request(self.api_url, json.dumps(params), header))
-        except urllib2.HTTPError, e:
-            if e.code:
-                response = e.fp
+            response = urllib2.urlopen(urllib2.Request(
+                self.api_url, json.dumps(params), header))
+        except urllib2.URLError, urllib2.HTTPError:
+            return u""
 
         json_data = response.read()
 
-        if all_response is True:
-            return json_data
-        else:
+        if not all_response:
             data = json.loads(json_data)
             if 'id' in data:
                 return json.loads(json_data)['id']
-            return ""
+            return u""
+        return json_data
 
     def expend(self, all_response = False):
-        json_data = urllib.urlopen("https://www.googleapis.com/urlshortener/v1/url?shortUrl=%s" % self.url).read()
+        json_data = urllib.urlopen(u"https://www.googleapis.com/urlshortener"
+                                   "/v1/url?shortUrl=%s" % self.url).read()
 
-        if all_response == True:
-            return json_data
-        else:
-            return json.loads(json_data)['longUrl'] if "longUrl" in json_data else ""
+        if not all_response:
+            return json.loads(
+                json_data)['longUrl'] if "longUrl" in json_data else ""
+        return json_data
